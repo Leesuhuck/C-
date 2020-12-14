ed7a7cfac02eb1e92e1a8c009c006161e3b89ab2
Leesuhuck <70427087+Leesuhuck@users.noreply.github.com>
2020-12-15 00:20:45 +0900

    Add files via upload

---
+++ C강의/Chapter1.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 실수형을 입력 받아서 소수점 셋째 자리까지 출력하기
+/*
+int main() {
+
+	double a;
+	scanf("%lf", &a);
+	printf("%.2f\n", a);
+	system("pause");
+	return 0;
+}
+*/
+
+// 두 숫자를 입력 받아 순서를 바꾸어 출력하기
+/*
+int main(void) {
+
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d %d\n", b, a);
+	system("pause");
+	return 0;
+}
+*/
+
+// 한 자리 씩 끊어서 입력 받기
+/*
+int main(void) {
+	int a, b, c;
+	scanf("%1d%1d%1d", &a, &b, &c);
+	printf("%d %d %d\n", a, b, c);
+	system("pause");
+	return 0;
+}
+*/
---
+++ C강의/Chapter2.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*
+// 사칙연산
+int main(void) {
+
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d + %d = %d\n", a, b, a + b);
+	printf("%d - %d = %d\n", a, b, a - b);
+	printf("%d * %d = %d\n", a, b, a * b);
+	printf("%d / %d = %d\n", a, b, a / b);
+	printf("%d %% %d = %d\n", a, b, a % b);
+	system("pause"); // 잠깐 정지했다가 프로그램이 종료되기위한 수단
+	return 0;
+}
+*/
+
+/*
+// Escape Sequence
+int main(void) {
+	
+	printf("\" A\tB\tC\tD\"\n");
+	printf("\" A\tB\tC\tD\"\n");
+	printf("\" A\tB\tC\tD\"\n");
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+// 관계연산자
+int main(void) {
+
+	int a, b;
+	scanf("%d %d", &a, &b);
+	printf("%d\n", a > b);
+	system("pause");
+	return 0;
+}
+*/
+
+/* 
+//논리 연산자
+int main(void) {
+
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d\n", !a);
+	printf("%d\n", a&&b);
+	printf("%d\n", (a > b) && (b > c));
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+//증감 연산자
+int main(void) {
+
+	int a = 7;
+	printf("%d\n", ++a);
+	printf("%d\n", a++);
+	printf("%d\n", ++a);
+	system("pause");
+	return 0;
+}
+*/
+
+/*
+//삼항 연산자
+int main(void) {
+
+	int a = 7, b = 7;
+	printf("%d\n", (a == b) ? 100 : -100);
+	system("pause");
+	return 0;
+}
+*/
